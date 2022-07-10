dd91d4eeeb62624a20dd5f66240247f71a01eb23
James Lucktaylor <jlucktay+github@gmail.com>
2022-07-10 17:52:14 +0100

    refactor(rpi-homelab/scripts/raspios64-release): cleanup

--- rpi-homelab/scripts/raspios64-release.sh
+++ rpi-homelab/scripts/raspios64-release.sh
@@ -7,44 +7,39 @@ set -euo pipefail
 ###     line 2: file size of download URL
 ### - if the '--download' flag is given, downloads the latest release
 
-# Get and parse HTML
-htmlgetnext() {
-  local IFS='>'
-  read -d '<' -r TAG VALUE
-}
-
-base_url="https://downloads.raspberrypi.org/raspios_arm64/images/"
-
 # Get the last (most recent(?)) link from the page
+base_url="https://downloads.raspberrypi.org/raspios_arm64/images/"
+curl_base=$(curl --silent "$base_url")
 declare raspios_release
 
-while htmlgetnext; do
+while IFS='>' read -d '<' -r TAG VALUE; do
   if [[ $TAG =~ raspios_arm64- ]]; then
     raspios_release=$VALUE
   fi
-done < <(curl --silent "$base_url")
+done <<< "$curl_base"
 
-if [ -z "${raspios_release:-}" ]; then
+if [[ -z ${raspios_release:-} ]]; then
   exit 1
 fi
 
 # Find the ZIP file to download
+curl_raspios=$(curl --silent "$base_url$raspios_release")
 declare zip_name
 
-while htmlgetnext; do
-  if [[ $VALUE =~ -arm64.zip$ ]]; then
+while IFS='>' read -d '<' -r TAG VALUE; do
+  if [[ $VALUE =~ -arm64.img.xz$ ]]; then
     zip_name=$VALUE
   fi
-done < <(curl --silent "$base_url$raspios_release")
+done <<< "$curl_raspios"
 
-if [ -z "${zip_name:-}" ]; then
+if [[ -z ${zip_name:-} ]]; then
   exit 2
 fi
 
 declare download_url="$base_url$raspios_release$zip_name"
 declare local_file="$HOME"/Downloads/raspios-arm64/"$zip_name"
 
-if [ "${1:-}" == "--download" ]; then
+if [[ ${1:-} == "--download" ]]; then
   mkdir -pv "$HOME"/Downloads/raspios-arm64
 
   wget \
